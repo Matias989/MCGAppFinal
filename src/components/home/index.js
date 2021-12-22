3ff78aa5989b54fcca0f4a66d05870dd943dc3fe
matias <matiaspena93@hotmail.com>
2021-12-22 02:39:18 -0300

    Se agrega navbar y se ajustan algunos paquetes

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import NavReact from "../navbar/index"
 
 const Home = () => {
     return (
-        <div>
-            Componente Home
-        </div>
+        <>
+            <NavReact></NavReact>
+        </>
     )
 }
 
